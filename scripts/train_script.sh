4925c550d3e2a2858ca9b8b9f80de3a19543504b
gkalstn000 <gkalstn000@gmail.com>
2023-06-08 16:04:51 +0900

    .

--- scripts/train_script.sh
+++ scripts/train_script.sh
@@ -4,14 +4,19 @@ nohup python train.py --tf_log --id classifier_flat --gpu_ids 3 --batchSize 40 -
 
 nohup python train.py --tf_log --id classifier_flat --gpu_ids 2 --batchSize 40 --num_workers 15  > classifier_flat.out &
 
-
+# RED
 nohup python train.py --id classifier_first_noaug --tf_log --gpu_ids 1 --batchSize 8 --num_workers 10 --no_augment > classifier_first_noaug.out &
-nohup python train.py --id classifier_first_aug --tf_log --gpu_ids 1 --batchSize 8 --num_workers 10 > classifier_first_aug.out &
+nohup python train.py --id classifier_first_aug --tf_log --gpu_ids 3 --batchSize 8 --num_workers 10 > classifier_first_aug.out &
+
+nohup python train.py --id classifier_second_noaug_finetune --tf_log --gpu_ids 1 --batchSize 8 --num_workers 10 --no_augment --continue_train --data_mode second > classifier_second_noaug_finetune.out &
+nohup python train.py --id classifier_second_aug_finetune --tf_log --gpu_ids 3 --batchSize 8 --num_workers 10 --continue_train --data_mode second > classifier_second_aug_finetune.out &
 
 
 
 
-nohup python train.py --id classifier_first_noaug --tf_log --gpu_ids 1 --batchSize 18 --num_workers 10 --dataroot /home/work/msha/el/  --no_augment > classifier_first_noaug.out &
+
+# NIPA
+nohup python train.py --id classifier_first_noaug --tf_log --gpu_ids 1 --batchSize 18 --num_workers 10 --dataroot /home/work/msha/el/  --no_augment  > classifier_first_noaug.out &
 nohup python train.py --id classifier_first_aug --tf_log --gpu_ids 1 --batchSize 18 --num_workers 10 --dataroot /home/work/msha/el/  > classifier_first_aug.out &
 
 nohup python train.py --id classifier_first_aug_16 --tf_log --gpu_ids 1 --batchSize 10 --num_workers 7 --nef 16 --dataroot /home/work/msha/el/  > classifier_first_aug_16.out &
